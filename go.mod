0d77170a0d25431608e20873239516623b36904b
NebuDev14 <wyun2006@gmail.com>
2021-08-18 22:26:44 -0400

    Create basic route

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module example.com/web-service-gin
+
+require github.com/gin-gonic/gin v1.7.4
--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
 type album struct {
 	ID		string	`json:"id"`
 	Title	string	`json:"title"`
@@ -7,3 +12,18 @@ type album struct {
 	Price	float64	`json:"price"`	
 }
 
+var albums = []album {
+	{ID: "1", Title: "His Theme", Artist: "Toby Fox", Price: 4.99},
+	{ID: "2", Title: "Introduce a little anarchy", Artist: "Hans Zimmer", Price: 4.99},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:3000")
+}
